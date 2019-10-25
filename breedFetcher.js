9cade7b29117822776a49c8102fb214f3b74c8b1
Russell Cape <russellcape98@gmail.com>
2019-10-25 19:44:11 +0000

    added the callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,21 +2,20 @@ const request = require('request');
 
 
 
-const fetchBreedDescription = function(breedName) {
-  breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, resp, body) => {
     if (error) {
-      console.log(`Failed to request details: ${error}`);
+      callback(`Failed to request details: ${error}`, null);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log(`Failed to find breed ${breedName}`);
+      callback(`Failed to find breed ${breedName}`, null);
     } else {
       const desc = data[0].description;
-      console.log(desc);
+      callback(null, desc);
     }
   });
 };
 
-fetchBreedDescription();
+module.exports = { fetchBreedDescription };
